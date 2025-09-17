b815b3d5ff54ad4973b413faee5a40eb791d822d
R SANDHIYA <sandhiyarameshkumar122@gmail.com>
2025-09-17 20:58:40 +0530

    Palindrome.js

---
+++ Palindrome.js
@@ -0,0 +1,9 @@
+
+let text = "mom"
+let reverseString = (text) => text.split("").reverse().join("");
+if(text.toLowerCase() == text.toLowerCase()) {
+console.log("String is palindrome")
+console.log(text)
+} else {
+    console.log("String is not an palindrome")
+}
